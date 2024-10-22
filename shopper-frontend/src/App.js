ceb8bf16f459276d3149fcb87375d905f7eee143
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-10-22 09:38:43 +0530

    initial routes and pages created

--- shopper-frontend/src/App.js
+++ shopper-frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import { Navbar } from './components/navbar/navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { ShopHome } from './pages/ShopHome';
+import { Product } from './pages/Product';
+import { Cart } from './pages/Cart';
+import { LoginSignup } from './pages/LoginSignup';
+import ShopCategory from './pages/ShopCategory';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<ShopHome/>}/>
+        <Route path='/mens' element={<ShopCategory category='men'/>}/>
+        <Route path='/womens' element={<ShopCategory category='women'/>}/>
+        <Route path='/kids' element={<ShopCategory category='kid'/>}/>
+        <Route path='/product'element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route> 
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ shopper-frontend/src/pages/ShopHome.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ShopHome = () => {
+  return (
+    <div><p>Gedra</p></div>
+  )
+}
