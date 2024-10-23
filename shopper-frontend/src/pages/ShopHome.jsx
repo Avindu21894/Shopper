e26fdbdc88ce1a1bf0f31f0f1334693fe4d9922b
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-10-23 08:17:38 +0530

    popular component created

--- shopper-frontend/src/pages/ShopHome.jsx
+++ shopper-frontend/src/pages/ShopHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Hero } from '../components/hero/hero'
+import { Popular } from '../components/popular/popular'
 
 export const ShopHome = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
     </div>
   )
 }
