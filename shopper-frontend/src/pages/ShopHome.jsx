912222346f1a0f6c57e2dd9b6044e51f603a9eee
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-10-25 07:49:08 +0530

    news letter component created

--- shopper-frontend/src/pages/ShopHome.jsx
+++ shopper-frontend/src/pages/ShopHome.jsx
@@ -3,6 +3,7 @@ import { Hero } from '../components/hero/hero'
 import { Popular } from '../components/popular/popular'
 import { Offers } from '../components/offers/offers'
 import { NewCollections } from '../components/newCollection/newCollections'
+import { NewsLetter } from '../components/news_letter/news_letter'
 
 export const ShopHome = () => {
   return (
@@ -11,6 +12,7 @@ export const ShopHome = () => {
       <Popular/>
       <Offers/>
       <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
