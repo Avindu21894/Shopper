0a65e49350242a4717ef0308fe31ccd26935c5ac
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-11-12 08:26:05 +0530

    login page

--- shopper-frontend/src/pages/LoginSignup.jsx
+++ shopper-frontend/src/pages/LoginSignup.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import './css/LoginSignup.css'
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign UP</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Emali Address'/>
+          <input type="password" placeholder='Password' />
+          <button>Continue</button>
+          <p className="loginsignup-login">Already have an account <span>Login Here</span></p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>By Continuing, I agree to the terms and privacy policy.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
