5d821236e73b633b87d1f874707626cdcbd70d3a
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-11-15 06:20:47 +0530

    breadcrumbs and product view created

---
+++ shopper-frontend/src/pages/Product.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../context/shop_context'
+import { useParams } from 'react-router-dom';
+import Breadcrum  from '../components/breadcrums/breadcrum';
+import ProductDisplay from '../components/productdisplay/productdisplay';
+
+export const Product = () => {
+  const {all_products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_products.find((e)=>e.id === Number(productId));
+  return (
+    <div>
+        <Breadcrum product={product}/>
+        <ProductDisplay product={product} />
+    </div>
+  )
+}
