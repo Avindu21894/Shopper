e9763dccb4ba2c1bb062a5781ef005fd8d282ad0
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-12-13 11:26:11 +0530

    API created

--- shopper-backend/index.js
+++ shopper-backend/index.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
+const { type } = require('os');
 
 app.use(express.json());
 app.use(cors());
@@ -14,7 +15,7 @@ app.use(cors());
 
 mongoose.connect("mongodb+srv://avindu:avindu12345678@cluster0.0kcf8.mongodb.net/e-commerce");
 
-//API Creation
+//----------API Creation-----------------   Test-Thunder client  --------------------------------
 
 app.get("/",(req, res) => {
     res.send("express app is running")
@@ -42,9 +43,102 @@ app.post("/upload", upload.single('product',(req,res)=>{
     })
 }))
 
+
+// Schema for creating products
+
+const Products = mongoose.model("Product", {
+    id:{
+        type: Number,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default:Date.now,
+    },
+    available:{
+        type: Boolean,
+        default: true,
+    },
+})
+
+
+// API for creating products
+
+app.post('/addproduct', async (req, res) => {
+    let products = await Products.find({})
+    let id;
+    if(products.length>0) 
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else
+    {
+        id=1;
+    }
+    const product = new Products({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product)
+    await product.save();
+    console.log("Product Created");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+
+// API for deleting products
+
+app.post('/removeproduct', async (req, res) =>{
+    await Products.findOneAndDelete({id:req.body.id});
+    console.log("Product Removed");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+
+// API for getting all products
+
+app.get('/allproducts', async (req, res) =>{
+    let products = await Products.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
+
+
 app.listen(port,(error) => {
     if(!error) {
-        console.log("Server running on port: " + port);
+        console.log("Server Running on Port: " + port);
     }
     else
     {
